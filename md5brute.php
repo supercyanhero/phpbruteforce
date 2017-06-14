05552d20bdf327a620498cf9c90e3176e0274e01
supercyanhero <noname21074@gmail.com>
2017-06-15 00:17:38 +0300

    Add files via upload

---
+++ md5brute.php
@@ -0,0 +1,19 @@
+﻿<?php
+$hash = $_GET['md5'];
+$passwordLength = $_GET['passlen'];
+$alphabetLength = $_GET['alphlen'];
+$numberOfTries = pow($alphabetLength, $passwordLength)-1;
+	
+	for($i = 0; $i < $numberOfTries; $i++)
+		{
+		if($hash == md5(base_convert($i, 10, $alphabetLength)))
+		{
+			echo "Your password is ".base_convert($i, 10, $alphabetLength);
+			return;
+		}
+	}
+	if($i>=$numberOfTries)
+		{
+			echo "Password not found";
+		}
+	?>
